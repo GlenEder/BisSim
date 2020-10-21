05d7d0c17da7a9925c990a26a8cabda2921afee4
GlenEder <geder@purdue.edu>
2020-10-21 01:01:34 -0400

    Displays businesses on client page

--- public/client.js
+++ public/client.js
@@ -10,4 +10,19 @@ async function getBusinesses () {
     jsonData.data.forEach(element => {
         console.log(element.BusName)
     });
+
+    let table = document.createElement('table')
+    jsonData.data.forEach(element => {
+        let row = document.createElement('tr')
+        let cell = document.createElement('td')
+        let text = document.createTextNode(element.BusName)
+        cell.appendChild(text)
+        row.appendChild(cell)
+
+        table.appendChild(row)
+    })
+
+    document.getElementById("dataTable").appendChild(table)
+
+
 }
