4327ad7fae52e85b14851e6bc71f08931dcf1703
GlenEder <geder@purdue.edu>
2020-10-20 20:57:14 -0400

    Sends landing page to user on load

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//use pulblic folder to send files from 
+app.use(express.static('public'))
+
+//send landing page to user
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.sendFile('index.html')
 })
 
 app.listen(port, () => {
-    console.log('Express app listening on http://localhost:${port}')
+    console.log('Express app listening on http://localhost:', port)
 })
