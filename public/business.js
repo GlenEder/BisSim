e8fe2eef2f3d236840843f2739258949868c28cd
GlenEder <geder@purdue.edu>
2020-11-16 23:42:31 -0500

    Adds business class file

---
+++ public/business.js
@@ -0,0 +1,14 @@
+
+
+class Business {
+
+    constructor(owner, id, name, dateFounded, city, state, address) {
+        this.owner = owner
+        this.id = id
+        this.dateFounded = dateFounded
+        this.city = city
+        this.state = state
+        this.address = address
+    }
+
+}
