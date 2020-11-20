c0ffad6559b9d78159d7de6fb6a58684be3a4a7f
gleneder <geder@purdue.edu>
2020-11-19 21:41:25 -0500

    Adds product class

---
+++ public/objects/product.js
@@ -0,0 +1,14 @@
+
+
+class Product {
+
+    constructor(item) {
+
+        this.id = item.ItemNum
+        this.typeId = item.TypeId
+        this.name = item.ItemName
+        this.brand = item.Brand
+
+    }
+
+}
