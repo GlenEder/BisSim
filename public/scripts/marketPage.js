fd372b63b10f66bd49b175f5518d9362d7866986
gleneder <geder@purdue.edu>
2020-11-19 21:44:25 -0500

    Puts queryied items into products array

--- public/scripts/marketPage.js
+++ public/scripts/marketPage.js
@@ -1,12 +1,21 @@
 //set onload for document
 window.addEventListener('load', displayProducts)
 
+//available products 
+let products = []
+
 //Display available products on table 
 async function displayProducts () {
 
     //call server for products 
     let dataReceived = await fetchServer('/getProducts', null)
     
+    let items = dataReceived.result
+    for(let i = 0; i < items.length; i++) {
+        products.push(new Product(items[i]))
+    }
+
+    console.log(products)
 
     //TODO display products on table
 
